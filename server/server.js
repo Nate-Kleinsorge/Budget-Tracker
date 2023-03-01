7b6123a609ab1662f8575f67535bcada80900045
Nate-Kleinsorge <110139749+Nate-Kleinsorge@users.noreply.github.com>
2023-03-01 12:19:42 -0500

    added apollo server

--- server/server.js
+++ server/server.js
@@ -1,16 +1,33 @@
-const path = require('path');
-const express = require('express');
+import http from 'http';
+import  path from 'path';
+import express from 'express';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import typeDefs from './schemas/typeDefs';
+import resolvers from './schemas/resolvers';
+
 const app = express();
+const httpServer = http.createServer(app);
 
 const PORT = process.env.PORT || 3002;
 const BUILD_PATH = path.resolve("../client/build");
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+})
+
+await server.start();
+
 app.use(express.static(BUILD_PATH))
+app.use('/graphql', express.json(), expressMiddleware(server));
 
 app.get("/ping", (req, res) => {
     res.send("pong");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
+httpServer.listen({  port: PORT }, () => {
+  console.log(`server running on port ${PORT}`)
 })
