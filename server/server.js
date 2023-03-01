f9ce335754c815f23cbc554a026903dcf0d0f309
Nate-Kleinsorge <110139749+Nate-Kleinsorge@users.noreply.github.com>
2023-02-28 19:26:44 -0500

    saved server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
-const PORT = 3002
+const PORT = process.env.PORT || 3002;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get("/ping", (req, res) => {
+    res.send("pong");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
