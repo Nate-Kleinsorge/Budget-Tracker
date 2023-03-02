bd9d800755bf14b147fbc494d0e318e0ce19fde7
Nate-Kleinsorge <110139749+Nate-Kleinsorge@users.noreply.github.com>
2023-03-02 11:09:53 -0500

    added budget form adn typedefs/resolvers for User

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,32 @@
- export default {
-    Query: {
-      ping() {
-        return {
-          message: "pong",
-          timestamp: new Date().toISOString(),
+import User from '../models/User.js';
+
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find();
+    },
+
+    user: async (parent, { userId }) => {
+      return User.findOne({ _id: userId });
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { email, password}) => {
+      return User.create({ email, password})
+    },
+
+    addUserData: async (parent, { userId, month, house, grocery, gas, stream, other}) => {
+      return User.findOneAndUpdate(
+        { _id: userId },
+        { $addToSet: { userData: {month, house, grocery, gas, stream, other} } },
+        { new: true,
+          runValidators: true,
         }
-      }
-    }
+      );
+    },
   }
+}
+
+export default resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,30 @@
-export default `#graphql
-  type Ping {
-    message: String
-    timestamp: String
-  } 
+const typeDefs =  `#graphql
+
+  type User {
+    _id: ID
+    email: String!
+    password: String!
+    userData: [Data]!
+  }
+
+  type Data {
+    _id: ID
+    monthIncome: String!
+    houseCost: String
+    groceryCost: String
+    gasCost: String
+    streamCost: String
+    otherCost: String
+  }
+
   type Query {
-    ping: Ping
+    users: [User]!
+    user(userId: ID!): User
+  }
+
+  type Mutation {
+    addUser(email: String!, password: String!): User
+    addUserData(userId: ID!, monthIncome: String!, houseCost: String, groceryCost: String, gasCost: String, streamCost: String, otherCost: String): User
   }
 `
+export default typeDefs;
