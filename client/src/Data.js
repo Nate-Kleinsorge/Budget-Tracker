4cb443d1a62492233e99a574ce380651638e48ee
unknown <bgandreev@gmail.com>
2023-03-09 13:01:43 -0600

    Build the dashboard landing page

---
+++ client/src/Data.js
@@ -0,0 +1,147 @@
+export const BarChartData = [
+    {
+        id: 1,
+        period: 'Jan 23',
+        monthlyIncome: 3000,
+        monthlyExpenses: 2450
+    },
+    {
+        id: 2,
+        period: 'Feb 23',
+        monthlyIncome: 7300,
+        monthlyExpenses: 4450
+    },
+    {
+        id: 3,
+        period: 'Mar 23',
+        monthlyIncome: 5000,
+        monthlyExpenses: 3600
+    },
+    {
+        id: 4,
+        period: 'Apr 23',
+        monthlyIncome: 0,
+        monthlyExpenses: 0
+    },
+    {
+        id: 5,
+        period: 'May 23',
+        monthlyIncome: 0,
+        monthlyExpenses: 0
+    },
+    {
+        id: 6,
+        period: 'Jun 23',
+        monthlyIncome: 0,
+        monthlyExpenses: 0
+    },
+    {
+        id: 7,
+        period: 'Jul 23',
+        monthlyIncome: 0,
+        monthlyExpenses: 0
+    },
+    {
+        id: 8,
+        period: 'Aug 23',
+        monthlyIncome: 0,
+        monthlyExpenses: 0
+    },
+    {
+        id: 9,
+        period: 'Sep 23',
+        monthlyIncome: 0,
+        monthlyExpenses: 0
+    },
+    {
+        id: 10,
+        period: 'Oct 23',
+        monthlyIncome: 0,
+        monthlyExpenses: 0
+    },
+    {
+        id: 11,
+        period: 'Nov 23',
+        monthlyIncome: 0,
+        monthlyExpenses: 0
+    },
+    {
+        id: 12,
+        period: 'Dec 23',
+        monthlyIncome: 0,
+        monthlyExpenses: 0
+    }
+];
+
+
+export const ExpenseData = [
+{
+id: 1,
+expenseCategory: 'Housing',
+totalAmount: 3000,
+},
+{
+id: 2,
+expenseCategory: 'Transportation',
+totalAmount: 900
+},
+{
+id: 3,
+expenseCategory: 'Food',
+totalAmount: 1200
+},
+{
+id: 4,
+expenseCategory: 'Utilities',
+totalAmount: 300
+},
+{
+id: 5,
+expenseCategory: 'Insurance & Healthcare',
+totalAmount: 700
+},
+{
+id: 6,
+expenseCategory: 'Recreation',
+totalAmount: 400
+},
+{
+id: 7,
+expenseCategory: 'Personal Spending',
+totalAmount: 650
+},
+{
+id: 8,
+expenseCategory: 'Miscellaneous',
+totalAmount: 300
+}
+];
+
+
+export const IncomeData = [
+    {
+        id: 1,
+        incomeCategory: 'Salary / Wages',
+        totalAmount: 3050
+    },
+    {
+        id: 2,
+        incomeCategory: 'Investment Income',
+        totalAmount: 750
+    },
+    {
+        id: 3,
+        incomeCategory: 'Interest Income',
+        totalAmount: 150
+    },
+    {
+        id: 4,
+        incomeCategory: 'Refunds / Reimbursements',
+        totalAmount: 125
+    },
+    {
+        id: 5,
+        incomeCategory: 'Other',
+        totalAmount: 700
+    }
+    ];
